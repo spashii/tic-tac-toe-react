3d688bf6ff5f23807fcedb3a9e973976e6b5fead
spashii <sameer.pashikanti@gmail.com>
2020-05-12 22:39:27 +0530

    Updates

---
+++ precache-manifest.01a5a8197e84967d3efca3f37cf5e774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29af76f8c8f902bc50a8e0443771931a",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ca927b8e65870c4f9130",
+    "url": "/tic-tac-toe/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "fd9bdddae594d9b68631",
+    "url": "/tic-tac-toe/static/js/2.260a02f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.260a02f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca927b8e65870c4f9130",
+    "url": "/tic-tac-toe/static/js/main.277d85ee.chunk.js"
+  },
+  {
+    "revision": "b3b5bb785f8fd2011d63",
+    "url": "/tic-tac-toe/static/js/runtime-main.1d78245a.js"
+  }
+]);
